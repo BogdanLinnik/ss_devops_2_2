09ec0c39150f535bfbbf17dd481d3467a83e5753
Bogdan Linnik <bogdan@geologie.com>
2025-03-14 10:54:12 +0200

    Provide basic configs for webpack

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,5 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/style.css",
+          to: "css/style.css"
+        },
+      ],
+    }),
+  ],
 };
